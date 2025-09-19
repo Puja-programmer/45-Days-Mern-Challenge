5298dfaf5539af0a52d3e1195bf78bb2f15cee98
Puja-programmer <pujapandey2406@gmail.com>
2025-09-20 02:37:23 +0530

    Update index.js

--- day14/server/index.js
+++ day14/server/index.js
@@ -17,4 +17,5 @@ app.use('/api/users', require('./routes/users'));
 app.use('/', express.static(path.join(__dirname, '..', 'public')));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log( ' Server running on port ${PORT}'));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
