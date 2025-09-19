df73966a861d029ee3c7974a111418e090e66a81
Puja-programmer <pujapandey2406@gmail.com>
2025-09-20 02:38:33 +0530

    index.js

--- day14/server/index.js
+++ day14/server/index.js
@@ -19,3 +19,4 @@ app.use('/', express.static(path.join(__dirname, '..', 'public')));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
+
