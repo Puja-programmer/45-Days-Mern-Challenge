fa25304404542aa4edc51e0502334a4d7fe05574
Puja-programmer <pujapandey2406@gmail.com>
2025-10-07 01:02:37 +0530

    Add files via upload

---
+++ Day16/server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+app.use(express.json());
+app.use(require('cors')());
+
+connectDB();
+
+app.use('/api/auth', require('./routes/auth'));
+
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on ${PORT}`));
